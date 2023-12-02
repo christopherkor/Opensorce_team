d5eef5c9009919bd0824128f9ac020e6b512c3f1
백혜경 <123654274+gPrudcomet@users.noreply.github.com>
2023-12-02 16:56:00 +0900

    Add files via upload

---
+++ PuzzlePiece.java
@@ -0,0 +1,12 @@
+public class PuzzlePiece {
+    private int face_value;
+
+    public int faceValue() {
+        return face_value;
+    }
+
+    public PuzzlePiece(int n) {
+        face_value = n;
+    }
+
+}
