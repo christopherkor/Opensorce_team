e4623f2e1d974229f1fab3898600c64cfd9a8b9b
백혜경 <123654274+gPrudcomet@users.noreply.github.com>
2023-12-02 17:01:02 +0900

    Update PuzzlePiece.java

--- PuzzlePiece.java
+++ PuzzlePiece.java
@@ -1,7 +1,7 @@
 public class PuzzlePiece {
     private int face_value; // 퍼즐 조각 위에 표시되는 값
 
-    public int faceValue() { // 조각의 액면 값 리턴
+    public int faceValue() { // 퍼즐 조각의 값 리턴
         return face_value;
     }
 
