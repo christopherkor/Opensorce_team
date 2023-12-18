32ff947cb93dcf64705bffadea734f56445a9739
백혜경 <123654274+gPrudcomet@users.noreply.github.com>
2023-12-18 22:29:31 +0900

    Add files via upload

---
+++ 4week/PuzzlePiece.java
@@ -0,0 +1,15 @@
+package _10_SlidePuzzleBoard;
+
+public class PuzzlePiece {
+    private int face;
+
+
+    public PuzzlePiece(int value) {
+        face = value;
+    }
+
+
+    public int face() {
+        return face;
+    }
+}
